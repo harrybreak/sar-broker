7ec8f8a7cdeeff865be0f18a93493e4404b1f141
Bryan <>
2024-09-17 22:58:02 +0200

    Finished echo server and echo client. Added a Test Main program to write code for simple usage

--- src/sar/TestEchoClient.java
+++ src/sar/TestEchoClient.java
@@ -1,8 +1,37 @@
 package sar;
 
 public class TestEchoClient {
-    // @SuppressWarnings("unused")
+	
+	public final static int N = 256;
+	public final static int PORT = 80;
+	
     public static void main(String args[]) {
+    	
+    	Broker clientBroker = new Broker("client");
         
+    	while (true) {
+    		
+    		byte data_sent[] = new byte[N];
+    		byte data_received[] = new byte[N];
+    		
+    		for (int i = 0 ; i < N ; i += 1)
+    			data_sent[i] = (byte)i;
+    		
+    		Channel c = clientBroker.connect("server", PORT);
+    		
+    		assert c != null : "Client broker cannot return channel";
+    		
+    		int sent = c.write(data_sent, 0, N);
+    		
+    		assert sent == N : "Client-side writing failed, data sent:"+sent;
+    		
+    		int received = c.read(data_received, 0, N);
+    		
+    		assert received == N : "Client-side reading failed, data read:"+received;
+    		
+    		for (int i = 0 ; i < N ; i += 1)
+    			assert data_sent[i] == data_received[i] : "Data sent is not equal to data received at "+i;
+    	}
+    	
     }
 }
--- src/sar/TestEchoServer.java
+++ src/sar/TestEchoServer.java
@@ -1,8 +1,28 @@
 package sar;
 
 public class TestEchoServer {
+	
+	public final static int N = 256;
+	public final static int PORT = 80;
     // @SuppressWarnings("unused")
     public static void main(String args[]) {
         
+    	Broker serverBroker = new Broker("server");
+    	
+    	while (true) {
+    		
+    		Channel c = serverBroker.accept(PORT);
+    		assert c != null : "ServerBroker cannot return channel";
+    		
+    		byte data_echoed[] = new byte[N];
+    		
+    		int data_read = c.read(data_echoed, 0, N);
+    		
+    		assert data_read == N : "Server-side reading failed, data read:"+data_read;
+    		
+    		int data_back = c.write(data_echoed, 0, N);
+    		
+    		assert data_back == N : "Server-side writing failed, data sent:"+data_back;
+    	}
     }
 }

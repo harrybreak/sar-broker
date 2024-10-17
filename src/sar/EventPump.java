36280bcb80230278609c2bcf04556fca08e8c281
Lilian Puech <lilian.puech@etu.univ-grenoble-alpes.fr>
2024-10-17 12:03:46 +0200

    I was not satisfied with this overcooking EventPump, so I have restored an older version but revised to be easier to understand

--- src/sar/EventPump.java
+++ src/sar/EventPump.java
@@ -1,77 +1,61 @@
 package sar;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class EventPump extends Thread {
-    
-    /**
-     * Singleton that represents the execution flow
-     * which executes all runnable events.
-     */
-    
-    private static final EventPump instance = new EventPump();
-    private final BlockingQueue<Runnable> queue;
-    private final AtomicBoolean running = new AtomicBoolean(false);
-
-    private EventPump() {
-    	
-        this.queue = new LinkedBlockingQueue<>();
-    }
-
-    public static EventPump inst() {
-    	
-    	instance.start();
-        return instance;
-    }
-
-    @Override
-    public void run() {
-    	
-        while (!Thread.currentThread().isInterrupted()) {
-        	
-            try {
-            	
-                Runnable r;
-                
-                synchronized (this) {
-                	
-                    while ((r = queue.poll()) == null && !running.get())
-                        wait();
-                }
-                
-                if (r != null)
-                    r.run();
-                
-            } catch (InterruptedException e) {
-            	
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    public synchronized void post(Runnable r) {
-    	
-        queue.offer(r);
-        notifyAll();
-    }
-
-    public synchronized void start() {
-    	
-        if (running.compareAndSet(false, true))
-        	super.start();
-    }
-
-    public synchronized void requestStop() {
-    	
-        running.set(false);
-        notifyAll();
-    }
-
-    public synchronized void interrupt() {
-    	
-        this.requestStop();
-        super.interrupt();
-    }
+	
+	/**
+	 * Singleton that represents the execution flow
+	 * which executes all runnable events.
+	 */
+	
+	Queue<Runnable> queue;
+	static EventPump instance;
+	
+	private EventPump() {
+		
+		this.queue = new LinkedList<Runnable>();
+	}
+	
+	public static EventPump inst() {
+		
+		synchronized (EventPump.class) {
+			
+			if (EventPump.instance == null) {
+				
+				EventPump.instance = new EventPump();
+				EventPump.instance.start();
+			}
+		}
+		
+		return instance;
+	}
+	
+	public synchronized void run() {
+		
+		while (true) {
+			
+			while (!this.queue.isEmpty())
+				
+				this.queue.remove().run();
+			
+			sleep();
+		}
+	}
+	
+	public synchronized void post(Runnable r) {
+		
+		this.queue.add(r); // at the end…
+		notify();
+	}
+	
+	private void sleep() {
+		
+		try {
+			wait();
+		} catch (InterruptedException ex){
+			// nothing to do here.
+		}
+	}
 }
